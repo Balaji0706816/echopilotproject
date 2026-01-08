e64e90f1ff8bd0ee9a2a62b2ee7e3932e2538f5c
Balaji Tanniru <balajitanniru@mac.sfw.umb.edu>
2026-01-08 13:55:14 -0500

    create Navbar

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex  items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Navbar />
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between  px-8 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-full flex-col items-center justify-between  px-8 bg-white dark:bg-black ">
 
       <FAQ />
       </main>
